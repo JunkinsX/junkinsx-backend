13a4d68ed6f1d3efffb2b6314231a4fe9728b1fd
Utej <91786744+utej8553@users.noreply.github.com>
2026-04-15 16:47:13 +0530

    added entityscan

--- src/main/java/com/example/jenkinsx/JenkinsXApplication.java
+++ src/main/java/com/example/jenkinsx/JenkinsXApplication.java
@@ -2,10 +2,14 @@ package com.example.jenkinsx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.jenkinsx.entity")
+@EnableJpaRepositories(basePackages = "com.example.jenkinsx.repository")
 public class JenkinsXApplication {
 
     public static void main(String[] args) {
